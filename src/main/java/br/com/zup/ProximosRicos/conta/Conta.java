1e6338eaf8b3e6a2969ef453ead6a61df3b9c5c0
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-26 15:55:39 -0300

    Criar classe Model

--- src/main/java/br/com/zup/ProximosRicos/conta/Conta.java
+++ src/main/java/br/com/zup/ProximosRicos/conta/Conta.java
@@ -1,4 +1,47 @@
 package br.com.zup.ProximosRicos.conta;
 
 public class Conta {
+    private String numeroConta;
+    private String agencia;
+    private Correntista correntista;
+    private Tipo tipo;
+
+    public Conta(String numeroConta, String agencia, Correntista correntista, Tipo tipo) {
+        this.numeroConta = numeroConta;
+        this.agencia = agencia;
+        this.correntista = correntista;
+        this.tipo = tipo;
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public Correntista getCorrentista() {
+        return correntista;
+    }
+
+    public void setCorrentista(Correntista correntista) {
+        this.correntista = correntista;
+    }
+
+    public Tipo getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Tipo tipo) {
+        this.tipo = tipo;
+    }
 }
