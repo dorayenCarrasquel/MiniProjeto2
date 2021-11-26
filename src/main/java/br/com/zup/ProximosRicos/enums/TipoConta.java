68dc7ce2359b84349e03805fe11f3e3f12e9bacc
Debora Souza <deboral.p@hotmail.com>
2021-11-26 16:06:51 -0300

    Modelar enum TipoConta

--- src/main/java/br/com/zup/ProximosRicos/enums/TipoConta.java
+++ src/main/java/br/com/zup/ProximosRicos/enums/TipoConta.java
@@ -1,4 +1,15 @@
 package br.com.zup.ProximosRicos.enums;
 
 public enum TipoConta {
+    CONTA_CORRENTE ("Conta Corrente"),
+    CONTA_POUPANCA("Conta Poupança");
+
+    private String descricao;
+    TipoConta (String descricao){
+        this.descricao = descricao;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+
 }
