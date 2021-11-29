a5f10e76320d14059c2364ad251f1b26e19d3270
Debora Souza <deboral.p@hotmail.com>
2021-11-29 08:37:35 -0300

    Modelar classe Extrato

--- src/main/java/br/com/zup/ProximosRicos/extrato/Extrato.java
+++ src/main/java/br/com/zup/ProximosRicos/extrato/Extrato.java
@@ -1,4 +1,20 @@
 package br.com.zup.ProximosRicos.extrato;
 
+import br.com.zup.ProximosRicos.evento.Evento;
+
+import java.util.List;
+
 public class Extrato {
+    private List<Evento>eventos;
+
+    public Extrato() {
+    }
+
+    public List<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void setEventos(List<Evento> eventos) {
+        this.eventos = eventos;
+    }
 }
