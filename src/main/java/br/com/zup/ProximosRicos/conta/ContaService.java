e2eb9f22b6bc511ebc5b98b7d33a1f1dc6973082
Debora Souza <deboral.p@hotmail.com>
2021-11-29 09:21:01 -0300

    Adicionar Anotação Service na classe

--- src/main/java/br/com/zup/ProximosRicos/conta/ContaService.java
+++ src/main/java/br/com/zup/ProximosRicos/conta/ContaService.java
@@ -1,7 +1,9 @@
 package br.com.zup.ProximosRicos.conta;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ContaService {
     @Autowired
     private ContaRepository contaRepository;
