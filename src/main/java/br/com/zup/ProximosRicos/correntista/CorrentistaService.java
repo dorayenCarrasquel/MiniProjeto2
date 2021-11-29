367e5ee2c04cd5ae9f6dcc8c31ba60a9fef37f1d
Debora Souza <deboral.p@hotmail.com>
2021-11-29 09:21:23 -0300

    Adicionar Anotação Service na classe

--- src/main/java/br/com/zup/ProximosRicos/correntista/CorrentistaService.java
+++ src/main/java/br/com/zup/ProximosRicos/correntista/CorrentistaService.java
@@ -1,7 +1,9 @@
 package br.com.zup.ProximosRicos.correntista;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CorrentistaService {
 
     @Autowired
