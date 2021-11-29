f2e98221a39c78c5a91a315c4ff04b7620f52163
Debora Souza <deboral.p@hotmail.com>
2021-11-29 09:13:18 -0300

    Injetar depedencia de EventoRepository em EventoService

--- src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
+++ src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
@@ -1,4 +1,9 @@
 package br.com.zup.ProximosRicos.evento;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EventoService {
+    @Autowired
+    private EventoRepository eventoRepository;
+
 }
