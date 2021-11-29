4db42da1189f735ba5811e4d6bc8833dc7a50071
Debora Souza <deboral.p@hotmail.com>
2021-11-29 11:36:10 -0300

    Modelar classe CadastroSaidaDTO

--- src/main/java/br/com/zup/ProximosRicos/conta/dto/CadastroSaidaDTO.java
+++ src/main/java/br/com/zup/ProximosRicos/conta/dto/CadastroSaidaDTO.java
@@ -1,4 +1,43 @@
 package br.com.zup.ProximosRicos.conta.dto;
 
+import br.com.zup.ProximosRicos.correntista.Correntista;
+import br.com.zup.ProximosRicos.enums.TipoConta;
+import br.com.zup.ProximosRicos.extrato.Extrato;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToOne;
+
 public class CadastroSaidaDTO {
+
+    private String numeroConta;
+    private Correntista correntista;
+    private TipoConta tipo;
+
+    public CadastroSaidaDTO() {
+    }
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Correntista getCorrentista() {
+        return correntista;
+    }
+
+    public void setCorrentista(Correntista correntista) {
+        this.correntista = correntista;
+    }
+
+    public TipoConta getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoConta tipo) {
+        this.tipo = tipo;
+    }
 }
