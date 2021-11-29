69d40fb07e2bc5028962203e7df90e76987adab7
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-29 11:47:16 -0300

    Criar PostMapping

---
+++ src/main/java/br/com/zup/ProximosRicos/conta/ContaController.java
@@ -0,0 +1,25 @@
+package br.com.zup.ProximosRicos.conta;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/caixa")
+public class ContaController {
+
+    @Autowired
+    private ContaService contaService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CadastroSaidaDTO cadastrarConta (@RequestBody @Valid CadastroEntradaDTO cadastroEntradaDTO){
+        Conta conta = modelMapper.map(cadastroEntradaDTO, Conta.class);
+        return modelMapper.map(contaService.salvarConta)
+    }
+}
