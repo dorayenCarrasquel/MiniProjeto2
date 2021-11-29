b8769a69cbd48e74cf248f168b7b284e8c8bb396
Debora Souza <deboral.p@hotmail.com>
2021-11-29 09:09:47 -0300

    Injetar depedencia de BancoRepository na Service

--- src/main/java/br/com/zup/ProximosRicos/banco/BancoService.java
+++ src/main/java/br/com/zup/ProximosRicos/banco/BancoService.java
@@ -1,4 +1,11 @@
 package br.com.zup.ProximosRicos.banco;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BancoService {
+    @Autowired
+    private BancoRepository bancoRepository;
+
 }
