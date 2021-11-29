e0f5248678b8ce6336d0205bf38d0b2d4cbc3b6c
Debora Souza <deboral.p@hotmail.com>
2021-11-29 09:21:38 -0300

    Adicionar Anotação Service na classe

--- src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
+++ src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
@@ -1,7 +1,9 @@
 package br.com.zup.ProximosRicos.evento;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EventoService {
     @Autowired
     private EventoRepository eventoRepository;
