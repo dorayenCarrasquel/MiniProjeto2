86f2a84075eee1b39c43f56ed6f936d9633f4b08
Debora Souza <deboral.p@hotmail.com>
2021-11-29 09:12:22 -0300

    Injetar depedencia de CorrentistaRepository em CorrentistaService

--- src/main/java/br/com/zup/ProximosRicos/correntista/CorrentistaService.java
+++ src/main/java/br/com/zup/ProximosRicos/correntista/CorrentistaService.java
@@ -1,4 +1,9 @@
 package br.com.zup.ProximosRicos.correntista;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CorrentistaService {
+
+    @Autowired
+    private CorrentistaRepository correntistaRepository;
 }
