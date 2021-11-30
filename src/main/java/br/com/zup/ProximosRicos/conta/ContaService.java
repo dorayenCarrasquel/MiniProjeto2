142b14f3b8ce71a7875777d601d11de8d3b489ca
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-30 11:30:49 -0300

    Buscar numero da conta by Id

--- src/main/java/br/com/zup/ProximosRicos/conta/ContaService.java
+++ src/main/java/br/com/zup/ProximosRicos/conta/ContaService.java
@@ -3,6 +3,8 @@ package br.com.zup.ProximosRicos.conta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class ContaService {
     @Autowired
@@ -11,4 +13,12 @@ public class ContaService {
     public Conta salvarConta (Conta conta){
         return contaRepository.save(conta);
     }
+    public Conta buscarConta(Integer numeroConta){
+        Optional<Conta> optionalConta = contaRepository.findById(numeroConta);
+        if (optionalConta.isEmpty()){
+            throw new RuntimeException("Conta não registrada");
+        }
+        return optionalConta.get();
+    }
+
 }
