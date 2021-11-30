cfa1eec74d0c34db82deb33bfc5229cb995c5c1d
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-30 09:10:40 -0300

    criar dto de entrada para os eventos

---
+++ src/main/java/br/com/zup/ProximosRicos/evento/dtos/EventoEntrada.java
@@ -0,0 +1,8 @@
+package br.com.zup.ProximosRicos.evento.dtos;
+
+import br.com.zup.ProximosRicos.enums.TipoEvento;
+
+public class EventoEntrada {
+    private TipoEvento tipoEvento;
+    private double valorEvento;
+}
