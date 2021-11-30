05af9e23e7b7ac93ae7b89c7d3c723dedb785e9f
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-30 14:11:15 -0300

    Criar pacotes de exceções personalizadas.

---
+++ src/main/java/br/com/zup/ProximosRicos/exceptions/ChequeEspecialException.java
@@ -0,0 +1,7 @@
+package br.com.zup.ProximosRicos.exceptions;
+
+public class ChequeEspecialException extends RuntimeException{
+    public ChequeEspecialException(String message) {
+        super(message);
+    }
+}
