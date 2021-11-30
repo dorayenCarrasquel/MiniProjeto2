1e2fd7472bfdbf06bab57cf722a8be8ce97e8cb4
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-30 10:12:07 -0300

    Criar construtor, getters e setters da classe ErroValidacao

--- src/main/java/br/com/zup/ProximosRicos/configs/ErroValidacao.java
+++ src/main/java/br/com/zup/ProximosRicos/configs/ErroValidacao.java
@@ -1,4 +1,27 @@
 package br.com.zup.ProximosRicos.configs;
 
 public class ErroValidacao {
+    private String campo;
+    private String mensagem;
+
+    public ErroValidacao(String campo, String mensagem) {
+        this.campo = campo;
+        this.mensagem = mensagem;
+    }
+
+    public String getCampo() {
+        return campo;
+    }
+
+    public void setCampo(String campo) {
+        this.campo = campo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
 }
