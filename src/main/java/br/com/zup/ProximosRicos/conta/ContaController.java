09947748866262092fd1f707b60b98ba65e08d32
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-30 11:28:33 -0300

    Aplicar Metodo PUT com os tipos de eventos

--- src/main/java/br/com/zup/ProximosRicos/conta/ContaController.java
+++ src/main/java/br/com/zup/ProximosRicos/conta/ContaController.java
@@ -2,6 +2,10 @@ package br.com.zup.ProximosRicos.conta;
 
 import br.com.zup.ProximosRicos.conta.dto.CadastroEntradaDTO;
 import br.com.zup.ProximosRicos.conta.dto.CadastroSaidaDTO;
+import br.com.zup.ProximosRicos.evento.Evento;
+import br.com.zup.ProximosRicos.evento.EventoService;
+import br.com.zup.ProximosRicos.evento.dtos.EventoEntrada;
+import br.com.zup.ProximosRicos.evento.dtos.EventoSaida;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,11 +21,25 @@ public class ContaController {
     private ContaService contaService;
     @Autowired
     private ModelMapper modelMapper;
+    @Autowired
+    private EventoService eventoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CadastroSaidaDTO cadastrarConta (@RequestBody CadastroEntradaDTO cadastroEntradaDTO){
         Conta conta = modelMapper.map(cadastroEntradaDTO, Conta.class);
         return modelMapper.map(contaService.salvarConta(conta), CadastroSaidaDTO.class);
+    }
+
+
+    @PutMapping("/{numeroConta}")
+    @ResponseStatus(HttpStatus.OK)
+    public EventoSaida aplicarEvento (@PathVariable int numeroConta, @RequestBody EventoEntrada eventoEntrada){
+        Conta conta = contaService.buscarConta(numeroConta);
+        Evento evento = modelMapper.map(eventoEntrada, Evento.class);
+        eventoService.aplicarEvento(conta, evento);
+        EventoSaida eventoSaida = modelMapper.map(conta, EventoSaida.class);
+
+        return eventoSaida;
     }
 }
