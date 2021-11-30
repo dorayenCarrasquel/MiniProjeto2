eae94ce7fa31ae5b41a8bf7254e4bc1737df32ec
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-30 11:32:42 -0300

    modificar Dto de saida do evento

--- src/main/java/br/com/zup/ProximosRicos/evento/dtos/EventoSaida.java
+++ src/main/java/br/com/zup/ProximosRicos/evento/dtos/EventoSaida.java
@@ -1,19 +1,14 @@
 package br.com.zup.ProximosRicos.evento.dtos;
-
 import br.com.zup.ProximosRicos.conta.Conta;
-import br.com.zup.ProximosRicos.enums.TipoEvento;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
 public class EventoSaida {
-    private Conta conta;
-    private TipoEvento tipoEvento;
-    private double valorEvento;
-    private double saldoInicial;
-    private double saldodisponivel;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime date;
+    private Conta conta;
+
+
 
     public EventoSaida() {
 
@@ -27,43 +22,4 @@ public class EventoSaida {
         this.conta = conta;
     }
 
-    public TipoEvento getTipoEvento() {
-        return tipoEvento;
-    }
-
-    public void setTipoEvento(TipoEvento tipoEvento) {
-        this.tipoEvento = tipoEvento;
-    }
-
-    public double getValorEvento() {
-        return valorEvento;
-    }
-
-    public void setValorEvento(double valorEvento) {
-        this.valorEvento = valorEvento;
-    }
-
-    public double getSaldoInicial() {
-        return saldoInicial;
-    }
-
-    public void setSaldoInicial(double saldoInicial) {
-        this.saldoInicial = saldoInicial;
-    }
-
-    public double getSaldodisponivel() {
-        return saldodisponivel;
-    }
-
-    public void setSaldodisponivel(double saldodisponivel) {
-        this.saldodisponivel = saldodisponivel;
-    }
-
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 }
