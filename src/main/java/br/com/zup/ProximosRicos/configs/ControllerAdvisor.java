1bed6d8a0278b55fb043211358ef9895fb7ae399
evellyinhelenoir <evellyin.nunes@zup.com.br>
2021-11-30 11:01:14 -0300

    Criar método de validação de erros na ControllerAdvisor

--- src/main/java/br/com/zup/ProximosRicos/configs/ControllerAdvisor.java
+++ src/main/java/br/com/zup/ProximosRicos/configs/ControllerAdvisor.java
@@ -1,8 +1,26 @@
 package br.com.zup.ProximosRicos.configs;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
 public class ControllerAdvisor {
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ErroValidacao> manipularErrosVal(MethodArgumentNotValidException exception) {
+        List<ErroValidacao> erros = new ArrayList<>();
+        for (FieldError fieldError : exception.getFieldErrors()) {
+            ErroValidacao erroValidacao = new ErroValidacao(fieldError.getField(), fieldError.getDefaultMessage());
+            erros.add(erroValidacao);
+        }
+        return erros;
+    }
 }
