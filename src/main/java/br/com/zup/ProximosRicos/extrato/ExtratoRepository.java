fe9aef77333a79c2fc6922cfb7055c7e45fd1375
Debora Souza <deboral.p@hotmail.com>
2021-11-30 08:29:24 -0300

    Deletar pacote extrato desnecessario para aplicacao

--- src/main/java/br/com/zup/ProximosRicos/extrato/ExtratoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.zup.ProximosRicos.extrato;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ExtratoRepository extends CrudRepository<Extrato,Integer> {
-
-}
