75789e0ec3696fee7e5fc5e0a5077f6dba3d6d41
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-30 14:59:29 -0300

    Separar metodos de eventos

--- src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
+++ src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
@@ -1,6 +1,7 @@
 package br.com.zup.ProximosRicos.evento;
 
 import br.com.zup.ProximosRicos.conta.Conta;
+import br.com.zup.ProximosRicos.conta.ContaRepository;
 import br.com.zup.ProximosRicos.enums.TipoEvento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,26 +12,29 @@ import java.time.LocalDateTime;
 public class EventoService {
     @Autowired
     private EventoRepository eventoRepository;
+    @Autowired
+    private ContaRepository contaRepository;
 
-    public void aplicarEvento (Conta conta, Evento evento) {
 
-        if (evento.getTipoEvento() == TipoEvento.SAQUE) {
-            if (evento.getSaldoDisponivel() < evento.getValorEvento()) {
-                throw new RuntimeException("Você entrou no cheque especial, por favor realize um deposito");
-            }
-            evento.setSaldoDisponivel(evento.getSaldoDisponivel()  - evento.getValorEvento());
-            conta.getEventos().add(evento);
+
+    public void aplicarSaque(Conta conta, Evento evento) {
+        if (evento.getSaldoDisponivel() < evento.getValorEvento()) {
+            throw new RuntimeException("Você entrou no cheque especial, por favor realize um deposito");
         }
-        else if (evento.getTipoEvento() == TipoEvento.DEPOSITO){
-            evento.setSaldoDisponivel(evento.getSaldoDisponivel()  + evento.getValorEvento());
-            conta.getEventos().add(evento);
-        }
-       // else if(evento.getTipoEvento() == TipoEvento.EXTRATO) {
-        //    conta.getEventos();
-      //  }
+        evento.setSaldoDisponivel(evento.getSaldoDisponivel() - evento.getValorEvento());
+        eventoRepository.save(evento);
+
+    }
+    public void aplicarDeposito(Conta conta, Evento evento) {
+        evento.setSaldoDisponivel(evento.getSaldoDisponivel() + evento.getValorEvento());
+        conta.getEventos().add(evento);
 
         evento.setData(LocalDateTime.now());
         eventoRepository.save(evento);
+
+    }
+
+    public void aplicarExtrato(Conta conta, Evento evento ){
 
     }
 
