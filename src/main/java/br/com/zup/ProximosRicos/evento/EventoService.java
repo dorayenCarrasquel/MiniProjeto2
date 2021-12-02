067ce173bded6aa3206be498bdda5d599e87e08c
Debora Souza <deboral.p@hotmail.com>
2021-12-02 14:55:14 -0300

    Modificar estrutura de injecao de dependencia 'Autowired'

--- src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
+++ src/main/java/br/com/zup/ProximosRicos/evento/EventoService.java
@@ -3,21 +3,24 @@ package br.com.zup.ProximosRicos.evento;
 import br.com.zup.ProximosRicos.conta.Conta;
 import br.com.zup.ProximosRicos.conta.ContaRepository;
 import br.com.zup.ProximosRicos.enums.TipoEvento;
-import br.com.zup.ProximosRicos.exceptions.ChequeEspecialException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class EventoService {
 
-    @Autowired
     private EventoRepository eventoRepository;
-    @Autowired
     private ContaRepository contaRepository;
 
-    public void gerarEvento(TipoEvento tipoEvento, double valorAtual, double valorEvento, Conta conta){
+    @Autowired
+    public EventoService(EventoRepository eventoRepository, ContaRepository contaRepository) {
+        this.eventoRepository = eventoRepository;
+        this.contaRepository = contaRepository;
+    }
+
+
+    public void gerarEvento(TipoEvento tipoEvento, double valorAtual, double valorEvento, Conta conta) {
         Evento evento = new Evento();
         evento.setTipoEvento(tipoEvento);
         evento.setSaldoDisponivel(valorAtual);
